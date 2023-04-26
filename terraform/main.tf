76b969b8f6dac5828e893aca9a6e910541344408
navnathbagade3 <navnathbagade3@gmail.com>
2023-04-26 17:50:51 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,27 @@
-module "resource_group" {
-  source = "api.env0.com/39137cc6-6aa7-48c6-b52d-f7a5e7748e87/mgm-af-azure-resource-group/azurerm"
-  #source = "git@github.com:MGMResorts/mgm-af-azure-resource-group.git?ref=v1" # Use if not env0, requires github access.
-  version  = "~> 1.0" # Comment out if using github source, pin to snapshot tag if tagged from develop
-  name     = join("-", [var.subject_name, module.globals.short_location, "rg", module.globals.short_environment])
-  location = "west us"
+terraform {
+  //create backend to store terraform state first - it cannot be created by this script
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.44.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  
+  resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = "West US"
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
 }
